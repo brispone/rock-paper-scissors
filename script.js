bc4767d59889c82d2acb1b7c836221174e0f964e
Bradley Rispone <bradrispone@gmail.com>
2023-03-02 00:09:09 -0600

    Add input validation & a function to play 5 rounds and declare a winner

--- script.js
+++ script.js
@@ -20,35 +20,89 @@ function getPlayerChoice() {
     // prompt the player to choose and store it in a variable
     let playerChoice = prompt("Choose rock, paper, or scissors: ");
 
-    // return the player's choice in all lower-case so that the input will be case-insensitive
-    return playerChoice.toLowerCase();
+    // convert player's choice to all lower-case so that the input will be case-insensitive
+    playerChoice = playerChoice.toLowerCase();
+
+    // make sure player choice is a valid game option - if it is, return the value, otherwise alert the user and prompt them again
+
+    if(validateChoice(playerChoice)) {
+        return playerChoice;
+    } else {
+        alert("That is not an option! Please choose rock, paper, or scissors: ");
+        return getPlayerChoice();
+    }
 }
 
+function validateChoice(choice){
+    //check to make sure the player input is a legal game option
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        return true;
+    } else return false;
+}
 
 function playRound(playerSelection, computerSelection) {
-    // compare the player and computer choices - build string saying who chose what
-    let choices = `You chose ${playerSelection}. The computer chose ${computerSelection}. `;
-    // append the game result to the choices string and return the message
-    // if they are they same return tie game message
+    // compare the player and computer choices and log them to the console
+    console.log(`You chose ${playerSelection}. The computer chose ${computerSelection}.`);
+    // log result of the game to console and return "win", "lose", or "tie" depending on the player's result
+    // if both choices are the same, it is a tie
     if (playerSelection === computerSelection) {
-        return choices + "It's a tie!";
+        console.log("It's a tie!");
+        return "tie";
     }
     // if player chose rock > if computer chose scissors, player wins, else they lose
     if (playerSelection === "rock") {
         if(computerSelection === "scissors") {
-            return choices + "You win!";
-        } else return choices + "You lose!";
+            console.log("You win!");
+            return "win";
+        } else  {
+            console.log("You lose!");
+            return "lose";
+        }
     }
     // if player chose paper > if computer chose rock, player wins, else they lose
     if (playerSelection === "paper") {
         if(computerSelection === "rock") {
-            return choices + "You win!";
-        } else return choices + "You lose!";
+            console.log("You win!");
+            return "win";
+        } else {
+            console.log("You lose!");
+            return "lose";
+        }
     }
     // if player chose scissors > if computer chose paper, player wins, else they lose
     if (playerSelection === "scissors") {
         if(computerSelection === "paper") {
-            return choices + "You win!";
-        } else return choices + "You lose!";
+            console.log("You win!");
+            return "win";
+        } else {
+            console.log("You lose!");
+            return "lose";
+        }
     }
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    
+    // play 5 rounds of the game
+    for (let i = 0; i < 5; i++) {
+        console.log(`Round ${i+1} (current score - You: ${playerScore} | Computer: ${computerScore})`);
+        let result = playRound(getPlayerChoice(), getComputerChoice());
+        if (result === "win") {
+            playerScore ++;
+        } else if (result === "lose") {
+            computerScore++;
+        }
+
+    }
+
+    // print results
+    console.log(`Final score - You: ${playerScore} | Computer: ${computerScore}`);
+    if (playerScore > computerScore) {
+        console.log("You win the game!");
+    } else {
+        console.log("The computer wins the game!");
+    }
+
 }
